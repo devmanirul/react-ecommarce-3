1610b4753d6a605747f68da9d81dc6de74a8dd7f
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2025-05-08 22:29:25 +0600

    ljkflakjs

--- src/components/Blogs/BlogCard/index.jsx
+++ src/components/Blogs/BlogCard/index.jsx
@@ -12,6 +12,14 @@ const BlogCard = ({ title, date, description }) => {
           <button className="btn btn-primary">Read More</button>
         </div>
       </div>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p className="text-sm text-gray-400">{date}</p>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Read More</button>
+        </div>
+      </div>
     </div>
   );
 };
